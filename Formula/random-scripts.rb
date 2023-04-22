4542ba23b7b386cd3658d1fbc9944e319624b588
amachang <seijro@gmail.com>
2023-04-22 23:07:13 +0900

    🥺

--- Formula/random-scripts.rb
+++ Formula/random-scripts.rb
@@ -6,12 +6,18 @@ class RandomScripts < Formula
   head "https://github.com/amachang/homebrew-random-scripts.git", :branch => "main"
   depends_on "python@3.9"
   depends_on "node@18"
+  depends_on "pixman"
+  depends_on "cairo"
+  depends_on "pango"
 
   def install
     venv = virtualenv_create(libexec)
-    venv.pip_install_and_link buildpath/"python"
+    venv.pip_install buildpath/"python"
 
-    system "npm", "install", buildpath/"node", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    chdir "node" do
+      system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    end
+    bin.install_symlink Dir["#{libexec}/bin/hello_node"]
+    bin.install_symlink Dir["#{libexec}/bin/hello_python"]
   end
 end
