ff8d5db204db1b585b101fd4b8a7c6414a7b5deb
Hitoshi Amano <seijro@gmail.com>
2023-08-13 00:30:06 +0900

    use python -m pip

--- Formula/random-scripts.rb
+++ Formula/random-scripts.rb
@@ -16,7 +16,7 @@ class RandomScripts < Formula
     venv_root = virtualenv_create(libexec).instance_variable_get("@venv_root")
 
     chdir "python" do
-      system venv_root/"bin/pip", "install", ".", "-v", "--ignore-installed"
+      system venv_root/"bin/python", "-m", "pip", "install", ".", "-v", "--ignore-installed"
     end
 
     chdir "node" do
